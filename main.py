72a751d1151c895a39c0fff1f03783df7e497292
Aakash Shrivastava <69726390+aakzsh@users.noreply.github.com>
2022-08-07 13:11:58 +0530

    add basic code

---
+++ main.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from time import time
+import tweepy as twitter
+import keys
+
+auth = twitter.OAuthHandler(keys.API_KEY, keys.API_SECRET_KEY)
+auth.set_access_token(keys.ACCESS_TOKEN, keys.SECRET_ACCESS_TOKEN)
+
+api = twitter.API(auth)
+
+def twitter_bot(tag, delay):
+    while True:
+        print("time is"+str(datetime.now))
+        # str()
+        for tweet in twitter.Cursor(api.search_tweets, q=tag, rpp=10).items(5):
+            try:
+                tweet_id = dict(tweet._json)['id']
+                tweet_text = dict(tweet._json)["text"]
+
+                print(tweet_id, tweet_text)
+
+                api.retweet(tweet_id)
+
+            except twitter.TwitterServerError as error:
+                print("some error occured", error)
+
+        time.sleep(delay)
+
+
+twitter_bot("hehe", 10)
